9eb7cbe2bf2b06de2a8c822545ef4a9a7b64819d
cakkash <cakkash@gmail.com>
2026-06-24 13:04:52 +0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,14 @@
-// for deploy on Vercel (auto-detects the platform)
-import adapter from '@sveltejs/adapter-auto';
-
-// for deploy on Ubuntu / NodeJS
-//import adapter from '@sveltejs/adapter-node';
-
-// for deploy on Cpanel 
-//import adapter from '@sveltejs/adapter-static';
+// for deploy on Vercel
+import adapter from '@sveltejs/adapter-vercel';
 
 import preprocessor from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocessor(),
 	preprocess: preprocessor({}),
 	kit: {
-		// for deploy on Cpanel
-		//adapter: adapter({ pages: 'build', fallback: "index.html", precompress: true }),
-
-		// for deploy on Ubuntu / NodeJS
 		adapter: adapter({}),
-	},
-	clientSideRendering: true,
-	clientSideRouting: true
+	}
 };
 
 export default config;
